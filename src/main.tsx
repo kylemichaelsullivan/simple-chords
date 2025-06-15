16676de42e2686c087d6ac5b4058a8a43349d7fa
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-06-15 00:33:26 -0400

    FIX: HMR <Index />

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { IndexContextProvider } from '@/context';
+import IndexContextProvider from '@/context';
 
 import App from '@/components/App';
 
