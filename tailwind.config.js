14020f2661cda151feac9b8c2a8528e02ce7baa9
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-10-02 23:01:06 -0400

    UPDATE: Project Deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /**  @type {import('tailwindcss').Config}  */
 export default {
 	content: ['./index.html', './src/**/*.{ts,tsx}'],
-	theme: { extend: {} },
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				17: 'repeat(17, minmax(0, 1fr))',
+			},
+		},
+	},
 	plugins: [],
-	safelist: [{ pattern: /grid-cols-\d+/ }],
 };
